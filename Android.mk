0b21905e379c4c025e2a60bee9530bc570ec4d0f
Zito <zzitoz@gmail.com>
2016-08-18 17:47:39 +0100

    fix camera build
    Change-Id: Iefa43ff6b951ce25ee935f9242cc4e029430ddb3

--- Android.mk
+++ Android.mk
@@ -13,10 +13,12 @@ MM_V4L2_DRIVER_LIST += msm8952
 MM_V4L2_DRIVER_LIST += msm8996
 MM_V4L2_DRIVER_LIST += msm8992
 
-ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
-  ifneq ($(strip $(USE_CAMERA_STUB)),true)
-    ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
+ifeq ($(strip $(BOARD_USES_AOSP_CAMERA)), true)
+  ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+    ifneq ($(strip $(USE_CAMERA_STUB)),true)
+      ifneq ($(BUILD_TINY_ANDROID),true)
+        include $(call all-subdir-makefiles)
+      endif
     endif
   endif
 endif
